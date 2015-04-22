e87c1effd0bbf6048627c24f63b9c1aae74b3357
Brett M <whohat@gmail.com>
2015-04-21 22:55:19 -0700

    Fixed touch dragging

--- static/js/drag.js
+++ static/js/drag.js
@@ -1,3 +1,14 @@
+var grid = document.getElementById('grid');
+var touchMoving = false;
+
+grid.addEventListener('dragstart', drag);
+grid.addEventListener('dragover', dragOver);
+grid.addEventListener('drop', drop);
+grid.addEventListener('dragend', dragEnd);
+
+grid.addEventListener('touchmove', touchMove);
+grid.addEventListener('touchend', touchEnd);
+
 function drag(event){
 
     event.dataTransfer.setData("text/plain", event.target.textContent);
@@ -16,21 +27,46 @@ function drop(event){
     var data = event.dataTransfer.getData("text/plain");
 
     event.target.textContent = data;
-    sendUpdate(event, data);
+    sendUpdate(event.target.id, data);
 }
 
 function dragEnd(event){
 
     if (event.dataTransfer.dropEffect === "move"){
         event.target.textContent = '';
-        sendUpdate(event, '');
+        sendUpdate(event.target.id, '');
     }
 }
 
-function sendUpdate(event, data){
+function sendUpdate(id, data){
 
     var update = {};
 
-    update[event.target.id] = data;
+    update[id] = data;
     ws.send(JSON.stringify(update));
+}
+
+function touchMove(event){
+
+    var sourceValue = event.target.textContent;
+
+    if (sourceValue === 'x'){
+        touchMoving = true;
+    }
+
+    event.preventDefault();
+}
+
+function touchEnd(event){
+    var destinationX = event.changedTouches[0].pageX;
+    var destinationY = event.changedTouches[0].pageY;
+    var destination = document.elementFromPoint(destinationX, destinationY);
+
+    if (touchMoving && destination.textContent !== 'x' && destination.nodeName === 'TD'){
+        touchMoving = false;
+        destination.textContent = 'x';
+        event.target.textContent = '';
+        sendUpdate(destination.id, 'x');
+        sendUpdate(event.target.id, '');
+    }
 }
